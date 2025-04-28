c4042664144ebbc6a6e15fac9d07b89c3ed65380
Luis Cadme <luisangel8876@outlook.es>
2025-04-28 17:05:33 -0500

    cambios en la base de datos cjas y estanterias

--- AdminTICS/Global.asax.cs
+++ AdminTICS/Global.asax.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Routing;
 
 namespace AdminTICS
 {
@@ -13,12 +10,7 @@ namespace AdminTICS
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            var routes = GlobalConfiguration.Configuration.Routes;
-            foreach (var route in routes)
-            {
-                Console.WriteLine(route.RouteTemplate);
-            }
-
+           
         }
     }
 }
